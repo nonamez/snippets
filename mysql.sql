65335e2016fbf84572dc02e2c5b422d9aa317caf
NoNameZ <nonamez123@gmail.com>
2013-10-08 00:47:22 +0300

    no message

--- mysql.sql
+++ mysql.sql
@@ -1,2 +1,10 @@
 # Order by "IN"
 SELECT * FROM `comments` WHERE `id` IN ('12','5','3','17') ORDER BY FIELD(`id`, '12','5','3','17')
+
+# Replace example
+UPDATE `wp_posts`
+SET `post_content` = REPLACE (
+	`post_content`,
+	'http://fjuz.net/wordpress/wp-content/uploads/',
+	'http://fjuz.net/wp-content/uploads/'
+)
